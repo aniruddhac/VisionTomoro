44bf3d7a89e9b3d224ce1e0bd0492d6118f423d6
aniruddha.c <caniruddha@hotmail.com>
2026-01-01 15:37:10 +0530

    page for enterprise mobililty added

--- nav.js
+++ nav.js
@@ -103,7 +103,8 @@ function loadNavigation() {
                   <li><a href="#">Product and Platforms</a></li>
                   <li><a href="#">Enterprise Architecture</a></li>
                   <li><a href="emerging-technologies.html">Emerging Technologies</a></li>
-                  <li><a href="#">Web and Mobile Application Development</a></li>
+                  <li><a href="#">Web, Cloud Native and Intelligent Apps</a></li>
+                  <li><a href="ent-mobility-and-connected-ent.html">Enterprise Mobility and Connected Enterprise</a></li>
                 </ul>
               </div>
             </div>
